341dc02f50ac3feab3ddd3ad0e587d43161afe60
cxh668 <105134793+cxh668@users.noreply.github.com>
2023-10-11 17:07:30 -0400

    Add files via upload

--- the-layered-era/src/presentation-layer/Home.js
+++ the-layered-era/src/presentation-layer/Home.js
@@ -1,15 +1,28 @@
 import InsertEvent from './InsertEvent';
 import InsertParticpant from './InsertParticipant';
+import DynamicTable from './Dynamictable';
+import React, { useState } from 'react';
+
 
 export default function Home() {
+    const [showTable, setShowTable] = useState(false);
+
+    const toggleTableVisibility = () => {
+        setShowTable((prevShowTable) => !prevShowTable);
+    };
+
     return (
         <div>
             <h1 style={{marginTop: '4%'}}>Rocking Bussin Layered Architecture :)</h1>
             <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-around', padding: '3rem'}}>
                 {/* Forms directly in the home page for now... maybe tabs or open on button click ? */}
                 <InsertEvent />
-                <InsertParticpant/>
+                <InsertParticpant/> 
             </div>
+            <button className = "viewEvents" onClick={toggleTableVisibility}>
+                {showTable ? 'Hide Events' : 'View Events'}
+            </button>
+            {showTable && <DynamicTable />}
         </div>
     );
 }
